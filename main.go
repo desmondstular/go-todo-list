998b5ebc3434b23c9aaa7e3b403af10ac76c1dc1
Desmond Stular <dstular@yahoo.com>
2025-03-06 22:44:10 -0700

    Added open + close file funcs with file locking

--- main.go
+++ main.go
@@ -38,3 +38,26 @@ func hasNoArgs(args []string) error {
 	}
 	return nil
 }
+
+
+func openFile(path string) (*os.File, error) {
+	fm, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file for reading")
+	}
+
+	// Exclusive lock obtained on file descriptor
+	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX); err != nil {
+		_ = f.Close()
+		return nil, err
+	}
+
+	return f, nil
+}
+
+
+func closeFile(f *os.File) error {
+	syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
+	return f.Close()
+}
