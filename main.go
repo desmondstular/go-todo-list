1a2cbc7b4955105318898e4fa4e5bff26d61b4dc
Desmond Stular <dstular@yahoo.com>
2025-03-06 22:37:26 -0700

    Added error handling when receiving 0 arguments

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ func main() {
 	// Get cmd line args
 	var args []string = os.Args[1:]
 
-	if len(args) == 0 {
+	// Check if no args passed
+	if err := hasNoArgs(args); err != nil {
+		fmt.Println(err)
 		return
 	}
 
@@ -27,4 +29,12 @@ func main() {
 	}
 
 	return
+}
+
+
+func hasNoArgs(args []string) error {
+	if n := len(args); n == 0 {
+		return fmt.Errorf("error: received no command")
+	}
+	return nil
 }
