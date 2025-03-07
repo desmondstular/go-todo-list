62ee2da5c791c67940f05f663444d57f5a2dbd04
Desmond Stular <dstular@yahoo.com>
2025-03-06 22:19:25 -0700

    Added cmd line arg reading and switch conditional

--- main.go
+++ main.go
@@ -7,7 +7,24 @@ import (
 
 func main() {
 	// Get cmd line args
-	args := os.args
+	var args []string = os.Args[1:]
 
+	if len(args) == 0 {
+		return
+	}
 
+	switch args[0] {
+	case "add":
+		fmt.Println(args[0])
+	case "list":
+		fmt.Println(args[0])
+	case "complete":
+		fmt.Println(args[0])
+	case "delete":
+		fmt.Println(args[0])
+	default:
+		fmt.Println("Not a valid command:", args[0])
+	}
+
+	return
 }
